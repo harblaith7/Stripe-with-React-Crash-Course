a637d3388ceece86ad43013e301cfee401f4a273
Laith Harb <harblaith77@gmail.com>
2020-07-19 15:03:15 -0400

    added server logic

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
-
+const Stripe = require('stripe')
+const keys = require('../config/dev')
+const dev = require('../config/dev')
 
 const app = express()
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Hello I am working")
+const stripe = new Stripe(dev.stripeSecret)
+
+app.post('/payment', async (req, res) => {
+   
+    try {
+        const {amount} = req.body
+        console.log(amount)
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "cad"
+        })
+
+        res.status(200).send(paymentIntent.client_secret);
+    } catch (error) {
+        res.status(500).json({ statusCode: 500, message: err.message });
+    }
+
 })
+
+
+module.exports = router
 
 app.listen(5000, () => {
     console.log("Now listening on PORT 5000")
