ab6b32cfda624b710182d383032a9c5c400c8559
Laith Harb <harblaith77@gmail.com>
2020-07-19 11:18:59 -0400

    Created react app and express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-hello
+const express = require('express')
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("Hello I am working")
+})
+
+app.listen(5000, () => {
+    console.log("Now listening on PORT 5000")
+})
